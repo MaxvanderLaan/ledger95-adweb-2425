be493665e3768a6da7cfd1bcadd762e2b904fc71
Gitfr <71840099+MaxvanderLaan@users.noreply.github.com>
2025-04-29 17:44:22 +0200

    homepage implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
+import styles from '@/app/home.module.css';
+import { HelveticaBQ, Helvetica } from '@/app/ui/fonts';
 
 export default function Home() {
   return (
-    <div><h1>Does text overlay still work?</h1></div>
+    <div className={styles.logo}>
+      <div>
+        <img className={styles.image} src="/w95-logo-big.png" />
+      </div>
+      <div className={styles.text}>
+        <div className={`${styles.company} ${Helvetica.className}`}>
+          Michealsoft
+        </div>
+        <div className={styles.title}>
+          <div className={`${styles.product} ${HelveticaBQ.className}`}>
+            Ledger
+          </div>
+          <div className={`${styles.number} ${Helvetica.className}`}>
+            95
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
